987c2703752d2c87bd71b755953d434a718cd40e
titiushadow <mesaque1212@gmail.com>
2023-04-15 11:04:03 -0300

    Fazendo tela de login e cadastro

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 function enviar() {
     document.getElementById("submitForm").reset();
 }
+
+function sair() {
+    const resposta = confirm("Você tem certeza que deseja sair?");
+    if (resposta == true) {
+        window.location.href = "../Login/login.html"; // redireciona para a página de login se a resposta for "sim"
+    } else {
+        return
+    }
+}
